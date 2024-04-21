84bf2f75b1f9bcf3d6d7276d41af237d26f1fd27
furkan9akin <160753076+furkan9akin@users.noreply.github.com>
2024-04-21 11:45:18 +0300

    Update discordbotu.py

--- discordbotu.py
+++ discordbotu.py
@@ -1,6 +1,8 @@
 import discord
 from discord.ext import commands
 import random
+import os
+import requests
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -70,5 +72,66 @@ async def sayi_tahmin_et(ctx,a=0):
         await ctx.send(f"Tahminin doğru sayıya {abs(a-x)} uzaklığında.")
     else:
         await ctx.send("Doğru sayıya uzaksın.")
+
+@bot.command()
+async def meme(ctx):
+    x=random.choice(os.listdir('discord_meme'))
+    with open(f'discord_meme/{x}', 'rb') as f:
+            picture = discord.File(f)
+            await ctx.send(file=picture)
+
+def get_duck_image_url():    
+    url = 'https://random-d.uk/api/random'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+
+@bot.command('duck')
+async def duck(ctx):
+    '''duck komutunu çağırdığımızda, program ordek_resmi_urlsi_al fonksiyonunu çağırır.'''
+    image_url = get_duck_image_url()
+    await ctx.send(image_url)
+
+#
+
+def get_fox_image_url():    
+    url = 'https://randomfox.ca/floof/'
+    res = requests.get(url)
+    data = res.json()
+    return data['image']
+
+@bot.command()
+async def fox(ctx):
+    '''duck komutunu çağırdığımızda, program ordek_resmi_urlsi_al fonksiyonunu çağırır.'''
+    image_url = get_fox_image_url()
+    await ctx.send(image_url)
+
+#
+
+def get_duck_image_url():    
+    url = 'https://random.dog/woof.json'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+
+@bot.command('dog')
+async def duck(ctx):
+    '''duck komutunu çağırdığımızda, program ordek_resmi_urlsi_al fonksiyonunu çağırır.'''
+    image_url = get_duck_image_url()
+    await ctx.send(image_url)
+
+#    
+
+def get_pikachu_image_url():    
+    url = ' https://pokeapi.co'
+    res = requests.get(url)
+    data = res.json()
+    return data['sprites']
+
+@bot.command()
+async def pikachu(ctx):
+    '''duck komutunu çağırdığımızda, program ordek_resmi_urlsi_al fonksiyonunu çağırır.'''
+    image_url = get_pikachu_image_url()
+    await ctx.send(image_url)
 
 bot.run("")
